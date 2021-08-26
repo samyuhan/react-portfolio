fbf53517814358d797622323e578dba01cd9547f
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-26 02:30:29 -0700

    create header

---
+++ app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const styles = {
+  heading: {
+    background: "aliceblue"
+    
+  },
+  h1: {
+    fontSize: "50px"
+  }
+};
+
+function Header() {
+  return (
+    <header style={styles.heading} className="header">
+      <h1 style={styles.h1}>Samantha Yuhan</h1>
+    </header>
+  );
+}
+
+export default Header;
