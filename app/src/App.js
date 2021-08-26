c06123b1ed23570e4c39324af13b5f339ff348f1
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-26 02:27:03 -0700

    add navbar

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Header />
     </div>
   );
 }
---
+++ app/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+const styles = {
+  nav: {
+    background: "aliceblue",
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+};
+
+function Navbar() {
+  return (
+    <nav style={styles.nav} className="navbar">
+      <a href="/">About Me</a>
+      <a href="/portfolio">Portfolio</a>
+      <a href="/contact">Contact</a>
+      <a href="Yuhan_Resume.pdf">Resume</a>
+    </nav>
+  );
+}
+
+export default Navbar;
