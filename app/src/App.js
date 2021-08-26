0f2323eaf7104e2e8ffd02d7ed6ce244d5a4728c
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-26 02:16:30 -0700

    populate App.js file

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
