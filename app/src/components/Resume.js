70ed8f9934acab44a155a4b17c9c2c85c6886457
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 21:08:51 -0700

    add styling to resume page

--- app/src/components/Resume.js
+++ app/src/components/Resume.js
@@ -12,8 +12,15 @@ const styles = {
       fontSize: "35px",
       color: "rgb(98, 98, 98)",
     },
+    h3: {
+      fontSize: "25px",
+      color: "rgb(98, 98, 98)",
+    },
     p: {
       marginTop: "30px",
+      marginBottom: "30px"
+    },
+    ul: {
       marginBottom: "0px"
     }
 }
@@ -23,7 +30,7 @@ function Resume() {
         <div style={styles.resume}>
             <h2 style={styles.h2}>Resume</h2>
             <p style={styles.p}>Download my <a href="Yuhan_Resume.pdf">resume</a></p>
-            <h3>Front-end Proficiencies</h3>
+            <h3 style={styles.h3}>Front-end Proficiencies</h3>
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -33,8 +40,8 @@ function Resume() {
                 <li>React</li>
                 <li>Bootstrap</li>
             </ul>
-            <h3>Back-end Proficiencies</h3>
-            <ul>
+            <h3 style={styles.h3}>Back-end Proficiencies</h3>
+            <ul style={styles.ul}>
                 <li>APIs</li>
                 <li>Node</li>
                 <li>Express</li>
