ed471fc73448dad214f00b3061a6671a669aaf66
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 20:31:31 -0700

    add github and deploy links to all projs

--- app/src/components/Portfolio.js
+++ app/src/components/Portfolio.js
@@ -26,40 +26,40 @@ function Portfolio() {
                 <img class="card-img" src="tama.jpg" alt="Card image"/>
                 <div class="card-overlay">
                     <h5 class="card-title">Tamagacha</h5>
-                    <button><a href="" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://tamagacha.herokuapp.com" className="link">Deployed</a></button><br/>
+                    <button><a href="https://github.com/samyuhan/Tamagacha" className="link">GitHub</a></button>
                 </div>
             </div>
             <div class="card" style={styles.workCard}>
                 <img class="card-img" src="pokemon.jpg" alt="Card image"/>
                 <div class="card-overlay">
                     <h5 class="card-title">Poke Battler</h5>
-                    <button><a href="" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://pokebattlerproject.herokuapp.com" className="link">Deployed</a></button><br/>
+                    <button><a href="https://github.com/samyuhan/pokebattler" className="link">GitHub</a></button>
                 </div>
             </div>
             <div class="card" style={styles.workCard}>
                 <img class="card-img" src="bored.png" alt="Card image"/>
                 <div class="card-overlay">
                     <h5 class="card-title">Feed Your Boredom</h5>
-                    <button><a href="" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://jessgiannini.github.io/FeedYourBoredom/" className="link">Deployed</a></button><br/>
+                    <button><a href="https://github.com/samyuhan/FeedYourBoredom" className="link">GitHub</a></button>
                 </div>
             </div>
             <div class="card" style={styles.workCard}>
                 <img class="card-img" src="password.png" alt="Card image"/>
                 <div class="card-overlay">
                     <h5 class="card-title">Password Generator</h5>
-                    <button><a href="" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://samyuhan.github.io/password-generator/" className="link">Deployed</a></button><br/>
+                    <button><a href="https://github.com/samyuhan/password-generator" className="link">GitHub</a></button>
                 </div>
             </div>
             <div class="card" style={styles.workCard}>
                 <img class="card-img" src="planner.png" alt="Card image"/>
                 <div class="card-overlay">
                     <h5 class="card-title">Planner</h5>
-                    <button><a href="" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://samyuhan.github.io/calendar/" className="link">Deployed</a></button><br/>
+                    <button><a href="https://github.com/samyuhan/calendar" className="link">GitHub</a></button>
                 </div>
             </div>
             <div class="card" style={styles.workCard}>
@@ -67,7 +67,7 @@ function Portfolio() {
                 <div class="card-overlay">
                     <h5 class="card-title">Fitness Tracker</h5>
                     <button><a href="https://fit-track-project.herokuapp.com/?id=611dde3847811a0016ac022a" className="link">Deployed</a></button><br/>
-                    <button><a href="" className="link">GitHub</a></button>
+                    <button><a href="https://github.com/samyuhan/workout-tracker" className="link">GitHub</a></button>
                 </div>
             </div>
         </div>
