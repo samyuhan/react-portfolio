4c2955c5f55bb37f8d1603b6c17a1cb7d77c1cc7
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 20:54:18 -0700

    add email button at footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -17,6 +17,7 @@ function Footer() {
     <footer style={styles.footing} className="footer">
       <a href="https://github.com/samyuhan"><button class="footerbtn"><i class="fa fa-github" style={styles.i}></i></button></a>
       <a href="https://www.linkedin.com/in/samanthayuhan/"><button class="footerbtn"><i class="fa fa-linkedin" style={styles.i}></i></button></a>
+      <a href="mailto:syuhan@berkeley.edu"><button class="footerbtn"><i class="fa fa-envelope" style={styles.i}></i></button></a>
     </footer>
   );
 }
