479f61d6abffa59b7b774a4c6463694849de870d
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 19:05:13 -0700

    add validation to contact form

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Header />
-      <About />
+      <Contact />
       <Footer />
     </div>
   );
--- app/src/components/Contact.js
+++ app/src/components/Contact.js
@@ -3,15 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = {
     formlayout: {
-      background: "aliceblue",
-      paddingTop: "75px",
-      paddingRight: "200px",
-      paddingLeft: "200px"
+        background: "aliceblue",
+        paddingTop: "75px",
+        paddingRight: "200px",
+        paddingLeft: "200px"
     },
     h2: {
-      fontSize: "35px",
-      color: "rgb(98, 98, 98)",
-      paddingBottom: "50px"
+        fontSize: "35px",
+        color: "rgb(98, 98, 98)",
+        paddingBottom: "50px"
     }
 };
 
@@ -22,15 +22,15 @@ function Contact() {
             <form class="row g-3">
                 <div class="col-md-6">
                     <label for="inputName" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="inputName" />
+                    <input type="text" class="form-control" id="inputName" required/>
                 </div>
                 <div class="col-md-6">
-                <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" />
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required/>
                 </div>
                 <div class="col-12">
                     <label for="inputMessage" class="form-label">Message</label>
-                    <textarea class="form-control" id="formControlTextMessage" rows="7" placeholder="Write here..."></textarea>
+                    <textarea class="form-control" id="formControlTextMessage" rows="7" placeholder="Write here..." required></textarea>
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Send</button>
